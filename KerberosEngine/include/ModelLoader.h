14bbd8814b53816bee66ae2064591081ff82a405
Luis R <78448904+Starmise@users.noreply.github.com>
2025-03-25 11:31:35 -0600

    FBX Integration

---
+++ KerberosEngine/include/ModelLoader.h
@@ -0,0 +1,44 @@
+#pragma once
+#include  "Prerequisites.h"
+#include  "MeshComponent.h"
+#include "fbxsdk.h"
+
+class
+  ModelLoader {
+public:
+  /**
+   * @brief Default constructor and destructor.
+   */
+  ModelLoader() = default;
+  ~ModelLoader() = default;
+
+  /**
+   * @brief
+   */
+  bool
+  LoadFBXModel(const std::string& filePath);
+
+  /**
+   * @brief
+   */
+  void
+  ProcessFBXNode(FbxNode* node);
+
+  /**
+   * @brief
+   */
+  void
+  ProcessFBXMesh(FbxNode* node);
+
+  /**
+   * @brief
+   */
+  std::vector<std::string>
+  ProcessFBXMaterials() const { return textureFileNames; }
+
+  std::vector<MeshComponent> meshes;
+private:
+  FbxManager* lSdkManager;
+  FbxScene* lScene;
+  std::vector<std::string> textureFileNames;
+};
